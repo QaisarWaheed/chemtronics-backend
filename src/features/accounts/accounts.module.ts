9d4b3446751e85dec12467a6771d8c0e65cbb217
unknown <qaiserwaheed00@gmail.com>
2025-09-13 15:48:25 +0500

    code fixed

---
+++ src/features/accounts/accounts.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CashbookController } from './cashBook/controllers/cashbook.controller';
+import { Cashbook, CashbookSchema } from './cashBook/entities/cashbook.entity';
+import { CashbookService } from './cashBook/services/cashbook.service';
+
+import {
+  JournalVoucher,
+  JournalVoucherSchema,
+} from './journalVoucher/entities/journal-voucher/journal-voucher';
+import { JournalvoucherService } from './journalVoucher/services/journalvoucher/journalvoucher.service';
+import { JournalVoucherController } from './journalVoucher/journal-voucher/journal-voucher.controller';
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Cashbook.name, schema: CashbookSchema },
+      { name: JournalVoucher.name, schema: JournalVoucherSchema },
+    ]),
+  ],
+  controllers: [CashbookController, JournalVoucherController],
+  providers: [CashbookService, JournalvoucherService],
+})
+export class AccountsModule {}
---
+++ src/features/accounts/journalVoucher/services/journalvoucher/journalvoucher.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JournalVoucher } from '../../entities/journal-voucher/journal-voucher';
+import { CreateJournalVoucherDto } from '../../dtos/create-journal-voucher/create-journal-voucher.dto';
+
+function parseMMDDYYYY(dateStr?: string): Date | undefined {
+  if (!dateStr) return undefined;
+  const regex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+  if (!regex.test(dateStr))
+    throw new BadRequestException('Date must be in mm/dd/yyyy format');
+  const [month, day, year] = dateStr.split('/');
+  const paddedMonth = month.padStart(2, '0');
+  const paddedDay = day.padStart(2, '0');
+  return new Date(`${year}-${paddedMonth}-${paddedDay}`);
+}
+
+@Injectable()
+export class JournalvoucherService {
+  constructor(
+    @InjectModel(JournalVoucher.name)
+    private journalVoucherModel: Model<JournalVoucher>,
+  ) {}
+
+  async create(createJournalVoucherDto: CreateJournalVoucherDto) {
+    return await this.journalVoucherModel.create(createJournalVoucherDto);
+  }
+
+  async findAll(filters?: { startDate?: string; endDate?: string }) {
+    const query: any = {};
+    let startDate: Date | undefined;
+    let endDate: Date | undefined;
+
+    if (filters?.startDate) {
+      startDate = parseMMDDYYYY(filters.startDate);
+      if (!startDate) throw new BadRequestException('Invalid start date');
+      startDate.setHours(0, 0, 0, 0);
+    }
+    if (filters?.endDate) {
+      endDate = parseMMDDYYYY(filters.endDate);
+      if (!endDate) throw new BadRequestException('Invalid end date');
+      endDate.setHours(23, 59, 59, 999);
+    }
+    if (startDate && endDate) {
+      query.date = { $gte: startDate, $lte: endDate };
+    } else if (startDate) {
+      query.date = { $gte: startDate };
+    } else if (endDate) {
+      query.date = { $lte: endDate };
+    }
+    return await this.journalVoucherModel.find(query).exec();
+  }
+
+  async findByVoucherNumber(voucherNumber: string) {
+    return await this.journalVoucherModel.findOne({ voucherNumber }).exec();
+  }
+}
