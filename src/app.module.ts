cd5c214c5982164826f26b45a99f69f72c6f1d67
QaisarWaheed <qaiserwaheed00@gmail.com>
2025-10-21 09:17:22 +0500

    configuration fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AuthModule } from './features/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -6,9 +5,13 @@ import { ProductsModule } from './features/products/products.module';
 import { StockOpeningModule } from './features/stock-opening/stock-opening.module';
 import { ChartOfAccountModule } from './features/ChartOfAccount/chart-of-account.module';
 import { InvoiceModule } from './features/invoices/invoice.module';
+import { ConfigModule } from '@nestjs/config';
 import { AccountsModule } from './features/accounts/accounts.module';
 @Module({
   imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI ?? '', {
       connectionName: 'test',
     }),
